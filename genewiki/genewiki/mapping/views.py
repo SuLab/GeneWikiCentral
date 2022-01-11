b3a8626ad5a5cb52e5e00c6bfbae8524b7b78df2
Andrew Su <andrew.su@gmail.com>
2022-01-11 21:52:59 +0000

    improve generality of sparql query (https://suwulab.slack.com/archives/C02G04Q6E/p1641771078004500?thread_ts=1641594836.003900&cid=C02G04Q6E)

--- genewiki/genewiki/mapping/views.py
+++ genewiki/genewiki/mapping/views.py
@@ -8,16 +8,21 @@ from genewiki.wiki.views import article_create
 
 def wiki_mapping(request, entrez_id):
     article_query = """
-        SELECT ?article WHERE {
-        ?cid wdt:P351 '"""+str(entrez_id)+"""'.
-        ?cid wdt:P703 wd:Q15978631 . 
-        OPTIONAL { ?cid rdfs:label ?label filter (lang(?label) = "en") .}
-        ?article schema:about ?cid .
-        ?article schema:inLanguage "en" .
-        FILTER (SUBSTR(str(?article), 1, 25) = "https://en.wikipedia.org/") . 
-        FILTER (SUBSTR(str(?article), 1, 38) != "https://en.wikipedia.org/wiki/Template")
-        } 
-        limit 1
+     SELECT ?article WHERE {
+             ?cid wdt:P351 '"""+str(entrez_id)+"""' ;
+                  wdt:P688 ?protein ;
+                  wdt:P703 wd:Q15978631 .
+             OPTIONAL { ?cid rdfs:label ?label filter (lang(?label) = "en") .}
+             OPTIONAL {?genearticle schema:about ?cid ;
+                          schema:inLanguage "en" .
+                       FILTER (SUBSTR(str(?genearticle), 1, 25) = "https://en.wikipedia.org/") .
+                       FILTER (SUBSTR(str(?genearticle), 1, 38) != "https://en.wikipedia.org/wiki/Template")}
+             OPTIONAL {?proteinarticle schema:about ?protein ;
+                          schema:inLanguage "en" .
+                      FILTER (SUBSTR(str(?proteinarticle), 1, 25) = "https://en.wikipedia.org/") .
+                      FILTER (SUBSTR(str(?proteinarticle), 1, 38) != "https://en.wikipedia.org/wiki/Template")}
+             BIND(COALESCE(?genearticle, ?proteinarticle) as ?article)
+             }
     """
     wikidata_results = wdi_core.WDItemEngine.execute_sparql_query(prefix=settings.PREFIX, query=article_query)['results']['bindings']
     article = ''
